a4a35dc4ef75a6ba81860c42451158434a3418f4
Juan Paredes <juanchuu.jfpc@gmail.com>
2018-12-12 13:56:10 +0100

    rails1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
 end
+
+
+#LE WAGON SOLUTION
+
+class ApllicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  #usamos esto para protegernos de hackers
+end
--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -11,3 +11,32 @@ class QuestionController < ApplicationController
 
   end
 end
+
+
+#LEWAGON SOLUTION
+
+
+class QuestionController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+
+    # it works just by using @question?
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can not hear you!"
+    elsif @question =~ / i am goin to work/i #es como usar REGEX, "=~", es para hacer match con al string, si la pregunta es
+    # i am going to work, si el user escribe eso, la respuesta será "GREAT"
+      @answer = "Great !"
+    else
+      @answer = "I do not care, get dressed and go to work!"
+    end
+
+
+  end
+
+
+
+end
