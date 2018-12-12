dbb64275822bf8ff228874c89a547f257c1c1f3b
Juan Paredes <juanchuu.jfpc@gmail.com>
2018-12-12 11:44:23 +0100

    first steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+ # get 'pages/show'
+  # the 'as:' :show lo ponemos para nombrar la ruta en 'rails routes'
+  get 'show', to: 'pages#show', as: :show
+
+  #get 'pages/update'
+  get 'update', to: 'pages#update', as: :update
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get pages_show_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get pages_update_url
+    assert_response :success
+  end
+
+end
